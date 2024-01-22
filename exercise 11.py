5f32a9b26081167f717c29e4f0e226aec72590fd
Ching <marcrohannmching@iskolarngbayan.pup.edu.ph>
2024-01-23 02:50:29 +0800

    finished code

--- exercise 11.py
+++ exercise 11.py
@@ -4,9 +4,9 @@
 number = 7536
 original_number = number
 
-reverse_number = 0
+
 while number > 0:
     remainder = number % 10 
-    reverse_number = (reverse_number * 10) + remainder
+
     number = number // 10
-print(reverse_number)
+    print(remainder, end=" ")
